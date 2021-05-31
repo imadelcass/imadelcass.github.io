fadcb9ee315597fe275c2afb3c4ff3cdf5d6a1e9
imad elcass <imadelcass@gmail.com>
2021-05-31 21:55:20 +0100

    update

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,61 +1,96 @@
-// const inner = document.querySelector('.inner');
-// const getData = async () => {
-//   const req = await fetch('https://yts.mx/api/v2/list_movies.json');
-//   const data = await req.json();
-//   return data.data.movies;
-// };
-// const pullData = async () => {
-//   let movies = await getData();
-//   movies.forEach(movie => {
-//     console.log(movie);
-//     let div = document.createElement('div');
-//     let img = document.createElement('img');
-//     let name = document.createElement('p');
-//     //append
-//     img.src = movie.large_cover_image;
-//     name.innerText = movie.title;
-//     //
-//     div.className = 'col-md-3'
-//     div.appendChild(img);
-//     div.appendChild(name);
-//     inner.appendChild(div);
-//   });
-// };
-// pullData();
-// //     // let home = document.createElement('div');
-// //     // let away = document.createElement('div');
-// //     // div.className = 'fixture';
-// //     // home.className = 'home';
-// //     // home.className = 'away';
-// //     // //logo of teams
-// //     // let imgHome = document.createElement('img');
-// //     // let imgAway = document.createElement('img');
-// //     // imgHome.src = fixture.teams.home.logo;
-// //     // imgAway.src = fixture.teams.away.logo;
-// //     // //name of teams
-// //     // let teamHome = document.createElement('h4');
-// //     // let teamAway = document.createElement('h4');
-// //     // teamHome.innerText = fixture.teams.home.name;
-// //     // teamAway.innerText = fixture.teams.away.name;
-// //     // //goals of teams
-// //     // let goalHome = document.createElement('h2');
-// //     // let goalAway = document.createElement('h2');
-// //     // goalHome.innerText = fixture.goals.home;
-// //     // goalAway.innerText = fixture.goals.away;
+const toggle = document.querySelector('.burger');
+const explore = document.querySelector('.explore');
+const nav = document.querySelector('nav');
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active');
+  nav.classList.toggle('navlist');
+});
+explore.addEventListener('click', () => {
+  window.scroll({ top: 1000, left: 0, behavior: 'smooth' });
+});
 
-// //     // home.appendChild(imgHome);
-// //     // home.appendChild(teamHome);
-// //     // home.appendChild(goalHome);
-// //     // away.appendChild(imgAway);
-// //     // away.appendChild(teamAway);
-// //     // away.appendChild(goalAway);
+//projects start
+const images = [
+  {
+    src: './img/project__menu.png',
+    href: 'http://127.0.0.1:5500/Sidebar/',
+  },
+  {
+    src: './img/project__trans.png',
+    href: 'http://127.0.0.1:5500/Transport/',
+  },
+  {
+    src: './img/project__portfolio.png',
+    href: 'http://127.0.0.1:5500/Portfolio/',
+  },
+  {
+    src: './img/project__menu.png',
+    href: 'http://127.0.0.1:5500/Menu/',
+  },
+  {
+    src: './img/project__menu.png',
+    href: 'http://127.0.0.1:5500/Menu/',
+  },
+  {
+    src: './img/project__menu.png',
+    href: 'http://127.0.0.1:5500/Menu/',
+  },
+];
+const display = document.querySelector('.display');
+const wrapper = document.querySelector('.wrapper');
+const project = document.querySelectorAll('.projects__single');
+const gotoProject = document.querySelector('.gotoProject');
 
-// //     // div.appendChild(home);
-// //     // div.appendChild(away);
+let currentProjectSelected = '';
+let img = document.createElement('img');
+project.forEach(item =>
+  item.addEventListener('click', () => {
+    currentProjectSelected = images[Number(item.id)].href;
+    img.src = images[Number(item.id)].src;
+    display.appendChild(img);
+    wrapper.style.display = 'initial';
+  })
+);
 
-// //     // // let p = document.createElement('p');
-// //     // // p.textContent = country.name;
-// //     // // div.appendChild(p);
-// //     // // img.src = country.flag;
-// //     // // div.appendChild(img);
-// //     // inner.appendChild(div);
+document.addEventListener('click', e => {
+  if (wrapper.contains(e.target)) {
+    display.removeChild(img);
+    wrapper.style.display = 'none';
+  }
+});
+gotoProject.addEventListener('click', e => {
+  gotoProject.href = currentProjectSelected;
+});
+//projects End
+
+// scroll to specifique area
+// const linkHome = document.querySelector('.link__home');
+// const linkProjects = document.querySelector('.link__projects');
+// const linkContact = document.querySelector('.link__contact');
+const links = document.querySelectorAll('li');
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    if (e.target.dataset.scroll == '1') {
+      window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+    } else if (e.target.dataset.scroll == '2') {
+      window.scroll({ top: 900, left: 0, behavior: 'smooth' });
+    } else if (e.target.dataset.scroll == '3') {
+      window.scroll({ top: 1600, left: 0, behavior: 'smooth' });
+    }
+  });
+});
+// return to top
+const returnTop = document.querySelector('.return__top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    returnTop.style.display = 'inline';
+  } else {
+    returnTop.style.display = 'none';
+  }
+});
+
+returnTop.addEventListener('click', () => {
+  window.scroll({ top: 0, behavior: 'smooth' });
+});
